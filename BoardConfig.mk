1d08c5a9743b113040e13a7efb63ef9f71fc01fb
nijel8 <nijel8@gmail.com>
2018-11-23 14:45:21 -0500

    Use system vold and services to decrypt userdata

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,10 +97,22 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 4095
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_EXCLUDE_TWRPAPP := true
+
+# Fix userdata decryption
+TARGET_RECOVERY_DEVICE_MODULES += dump_footer
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd \
+    hwservicemanager \
+    keymaster-3-0 \
+    servicemanager \
+    vndservicemanager \
+    gatekeeper-1-0 \
+    keystore
+
+#TW_CRYPTO_SYSTEM_VOLD_DEBUG := /system/xbin/strace
+#TWRP_INCLUDE_LOGCAT := true
+#TARGET_USES_LOGD := true
